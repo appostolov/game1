a31e39f5418fd8c335d726f6945e0e51d9c011df
appostolov <appostolov@gmail.com>
2014-05-31 16:34:58 +0300

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,201 +1,297 @@
-var can = document.getElementById("canvas");
-var c = can.getContext("2d");
+var c1 = document.getElementById("can");
+var c = c1.getContext("2d");
 
-var back_color = "gray";
-var t_color = "green";
+//BACKGROUND COLOR
+var back_color = "#3cf";
 
-var num = 6;
-var hits = 0;
+//BALL object
+var ball = new Object();
+ball.x = c1.width/2;
+ball.y = 55;
+ball.r = 15;
+ball.color = "red";
+ball.dir = 90;
+ball.vell = 3;
 
-var x = 100;
-var y = 100;
-var w = 20;
-var h = 20;
-var vel_x = 2;
-var vel_y = 2;
+//BASE object
+var base = new Object();
+base.x = 0;
+base.height = 30;
+base.y = c1.height - base.height;
+base.width = 100;
+base.color = "black";
+base.vell = 3;
 
-var b_x = 200;
-var b_y = 470;
-var b_w = 100;
-var b_h = 25;
-var b_vell = 5;
-var m_l = false;
-var m_r = false;
-
-var t_x_num = 2;
-var t_x = new Array;
-var t_y = new Array;
-var t_w = 81;
-var t_h = 25;
-var t_live = new Array;
-for(var b = 0; b<num; b++){
-	t_x[b] = t_x_num+b*83;
-	t_y[b] = 2;
-	t_live[b] = true;
-}
-
+//KEY DOWN EVENT
 document.addEventListener('keydown', function(event){
 	
 	var key_press = String.fromCharCode(event.keyCode);
-	if(key_press == "D" && b_x < can.width-b_w){
+	if(key_press == "D" && base.x < c1.width-base.width){
 		m_r = true;
 	}
 	
-	if(key_press == "A" && b_x > 0){
+	if(key_press == "A" && base.x > 0){
 		m_l = true;
 	}
 	
 });
 
+//KEY UP EVENT
 document.addEventListener('keyup', function(event){
 	
 	var key_press = String.fromCharCode(event.keyCode);
-	if(key_press == "D" && b_x < can.width-b_w){
+	if(key_press == "D"){
 		m_r = false;
 	}
 	
-	if(key_press == "A" && b_x > 0){
+	if(key_press == "A" ){
 		m_l = false;
 	}
 	
 });
 
+var num = 6;
+var tar = new Array();
+var sub_x;
+var sub_y;
 
-setInterval(function(){
+for(var a = 0; a < num; a++){
+	tar[a] = new Object();
+	tar[a].x = 2 + a*83;
+	tar[a].y = 2;
+	tar[a].w = 81;
+	tar[a].h = 37;
+	tar[a].live = true;
+	tar[a].color = "green";
+}
 
-		c.fillStyle = back_color;
-		c.fillRect(0,0,can.width,can.height);
-		
-		c.fillStyle = "red";
-		c.fillRect(x,y,w,h);
-		
-		c.fillStyle = "blue";
-		c.fillRect(b_x,b_y,b_w,b_h);
-		
-		for(var a = 0; a < num; a++){
-			
-			if(t_live[a] == true){
-			c.fillStyle = t_color;
-			c.fillRect(t_x[a],t_y[a],t_w,t_h);
+//MAIN GAME LOOP
+var game_loop = setInterval(function(){
+	
+	var count = 0;
+
+	//BACKGROUND
+	var img=document.getElementById("img");
+	c.drawImage(img,0,0);
+	
+	var brick_view=document.getElementById("brick_img");
+	for(var a = 0; a < num; a++){
+		if(tar[a].live == true){
+			c.drawImage(brick_view,tar[a].x,tar[a].y);
+		}	
+	}
+
+	//BALL
+	c.fillStyle = "rgba(0,0,0,0)";
+	c.beginPath();
+	c.arc(ball.x,ball.y,ball.r,0,2*Math.PI);
+	c.fill();
+	
+	var ball_view=document.getElementById("ball_img");
+	c.drawImage(ball_view,ball.x-ball.r,ball.y-ball.r);
+	
+	//BALL movement
+	ball.x += ball.vell*Math.cos(ball.dir*Math.PI/180);
+	ball.y += ball.vell*Math.sin(ball.dir*Math.PI/180);
+	
+	//BALL hit right wall
+	if(ball.x >= c1.width - ball.r){
+		ball.x = c1.width - ball.r;
+		ball.dir = 180 - ball.dir;
+	}
+	
+	//BALL hit left wall
+	if(ball.x <= ball.r){
+		ball.x = ball.r;
+		ball.dir = 180 - ball.dir;
+	}
+	
+	//BALL hit top
+	if(ball.y <= ball.r){
+		ball.y = ball.r;
+		ball.dir = 360 - ball.dir;
+	}
+	
+	//BALL hit the bottom
+	if(ball.y >= c1.height - ball.r){
+		ball.y = c1.height - ball.r;
+		ball.dir = 360 - ball.dir;
+	}
+
+	//BASE
+	var base_view = document.getElementById("base_img");
+	c.drawImage(base_view,base.x,base.y);
+	
+	//BASE hit left wall
+	if(base.x < 0){
+		base.x = 0;
+	}
+	
+	//BASE hit the right wall
+	if(base.x > c1.width - base.width){
+		base.x = c1.width - base.width;
+	}
+	
+	//BASE left corner to ball lenght
+	var base_left_ball_lenght = Math.sqrt(Math.pow(base.y-ball.y,2)+Math.pow(base.x-ball.x,2));
+	//BASE left corner to ball direction
+	var base_left_ball_dir = Math.atan2(base.y-ball.y,base.x-ball.x)*180/Math.PI;
+	//BASE hit the ball with left corner
+	if(base_left_ball_lenght <= ball.r && base_left_ball_dir < 90){
+		ball.dir = 180 + base_left_ball_dir;
+	}
+	
+	//BASE right corner to ball lenght
+	base_right_ball_lenght = Math.sqrt(Math.pow(base.y-ball.y,2)+Math.pow(base.x+base.width-ball.x,2));
+	//BASE right corner to ball direction
+	base_right_ball_dir = Math.atan2(base.y-ball.y,base.x+base.width-ball.x)*180/Math.PI;
+	//BASE hit the ball with right corner
+	if(base_right_ball_lenght <= ball.r && base_right_ball_dir > 90){
+		ball.dir = 180 + base_right_ball_dir;
+	}
+	
+	//BASE hit the ball with top side
+	if(base_left_ball_dir >= 90 && base_right_ball_dir <= 90 && ball.y >= base.y - ball.r){
+		ball.y = base.y - ball.r;
+		ball.dir = 360 - ball.dir;
+		if(m_l == true){
+			ball.dir -= 30;
+		}
+		if(m_r == true){
+			ball.dir += 30;
+		}
+	}
+	
+	//BASE hit the ball with left or right side
+	if(ball.y > base.y){
+		if(base_left_ball_dir >= -90 && base_left_ball_dir <= 0 &&ball.x > base.x - ball.r){
+			if(m_l == true){
+				ball.x = base.x - ball.r - base.vell;
+				ball.dir = 180 - ball.dir;
+			}else{
+				ball.x = base.x - ball.r;
+				ball.dir = 180 - ball.dir;
 			}
 		}
-		
-		x+=vel_x;
-		y+=vel_y;
-		
-		if(x < 0 || x > can.width - w){
-			vel_x = -vel_x;
+		if(base_right_ball_dir <= -90 && base_right_ball_dir <= 0 && ball.x < base.x + base.width + ball.r){
+			if(m_r == true){
+				ball.x = base.x + base.width + ball.r + base.vell;
+				ball.dir = 180 - ball.dir;
+			}else{
+				ball.x = base.x + base.width + ball.r;
+				ball.dir = 180 - ball.dir;
+			}
 		}
-		
-		if(y < 0){
-			vel_y = -vel_y;
-		}
-		
-		if(b_x-b_vell <0){
-			b_x = 0;
+		if(ball.x <= ball.r && base.x <= ball.x + ball.r){
+			base.x = ball.x + ball.r;
 			m_l = false;
 		}
-		
-		if(b_x+b_w+b_vell > can.width){
-			b_x = can.width - b_w;
+		if(ball.x >= c1.width - ball.r && base.x >= ball.x - ball.r - base.width){
+			base.x = ball.x - ball.r - base.width;
 			m_r = false;
 		}
-		
-		if(m_r == true){
-			b_x += b_vell;
-		}
-		
-		if(m_l == true){
-			b_x -= b_vell;
-		}
-		
-		if(x+w > b_x && x < b_x+b_w){
-			if(y+h > b_y && y < b_y+b_h){
-				vel_y = -vel_y;
-				if(m_r == true){
-					vel_x += 1;
-				}
-				if(m_l == true){
-					vel_x -= 1;
-				}
-			}
-		}
-		
-		if(y+h > b_y && y < b_y+b_h){
-			if(x+w == b_x || x == b_x+b_w){
-				vel_x = -vel_x;
-				if(m_r == true){
-					vel_x += 1;
-				}
-				if(m_l == true){
-					vel_x -= 1;
-				}
-			}
-		}
-		
-		for(var b = 0; b<num; b++){
-			if( t_live[b] == true){
-				if(x<t_x[b]+t_w && x+w>t_x[b]){
-					if(y<t_y[b]+t_h && y+w>t_y[b]){
-						vel_y = -vel_y;
-						t_live[b] = false;
-						hits += 1;
-					}	
-				}
-			}
-			
-		}
-		
-		if(y+h > can.height){
-			back_color = "gray";
-			t_color = "green";
-			num = 6;
-			hits = 0;
-			x = 100;
-			y = 100;
-			w = 20;
-			h = 20;
-			vel_x = 2;
-			vel_y = 2;
-			
-			for(var b = 0; b<num; b++){
-				t_live[b] = true;
-			}
-			
-			hits = 0;
-
-		}
-		
-		if(hits == num){
-			num += 6;
-			if(num == 12){
-				back_color = "#222";
-				t_color = "yellow";
-			}
-			if(num == 18){
-				back_color = "#000";
-				t_color = "red";
-			}
-			hits = 0;
-			vel_x += 1.05*vel_x;
-			vel_y += 1.05*vel_y;
-			b_vell += 1;
-			var u = 0;
-			var o = 0;
-			for(var b = 0; b<num; b++){
-				if(u == 6){
-					u = 0;
-					o+=27;
-				}
-				t_x[b] = t_x_num+u*83;
-				t_y[b] = o + 2;
-				c.fillStyle = "yellow";
-				c.fillRect(t_x[b],t_y[b],t_w,t_h);
-				t_live[b] = true;
-				u++;
-			}
-			y = o + 35;
-			
-		}
+	}
 	
-	},16);
+	for(var a = 0; a < num; a++){
+		
+		if(tar[a].live == true){
+			var tar_top_left_lenght = Math.sqrt(Math.pow(tar[a].y-ball.y,2)+Math.pow(tar[a].x-ball.x,2));
+			var tar_top_left_dir = Math.atan2(tar[a].y-ball.y,tar[a].x-ball.x)*180/Math.PI;
+			
+			var tar_top_right_lenght = Math.sqrt(Math.pow(tar[a].y-ball.y,2)+Math.pow(tar[a].x+tar[a].w-ball.x,2));
+			var tar_top_right_dir = Math.atan2(tar[a].y-ball.y,tar[a].x+tar[a].w-ball.x)*180/Math.PI;
+			
+			var tar_bottom_left_lenght = Math.sqrt(Math.pow(tar[a].y+tar[a].h-ball.y,2)+Math.pow(tar[a].x-ball.x,2));
+			var tar_bottom_left_dir = Math.atan2(tar[a].y+tar[a].h-ball.y,tar[a].x-ball.x)*180/Math.PI;
+			
+			var tar_bottom_right_lenght = Math.sqrt(Math.pow(tar[a].y+tar[a].h-ball.y,2)+Math.pow(tar[a].x+tar[a].w-ball.x,2));
+			var tar_bottom_right_dir = Math.atan2(tar[a].y+tar[a].h-ball.y,tar[a].x+tar[a].w-ball.x)*180/Math.PI;
+			
+			if(tar_top_left_lenght <= ball.r){
+				tar[a].live = false;
+				ball.dir = 180 + tar_top_left_dir;
+			}
+			
+			if(tar_top_right_lenght <= ball.r){
+				tar[a].live = false;
+				ball.dir = 180 + tar_right_left_dir;
+			}
+			
+			if(tar_bottom_left_lenght <= ball.r){
+				tar[a].live = false;
+				ball.dir = 180 + tar_bottom_left_dir;
+			}
+			
+			if(tar_bottom_right_lenght <= ball.r){
+				tar[a].live = false;
+				ball.dir = 180 + tar_bottom_right_dir;
+			}
+			
+			if(tar_bottom_left_dir < -90 && tar_bottom_right_dir > -90 && ball.y < tar[a].y + tar[a].h + ball.r){
+				tar[a].live = false;
+				ball.y = tar[a].y + tar[a].h + ball.r;
+				ball.dir = 360 - ball.dir;
+			}
+			
+			if(tar_top_left_dir > 90 && tar_top_right_dir < 90 && ball.y > tar[a].y - ball.r){
+				tar[a].live = false;
+				ball.y = tar[a].y - ball.r;
+				ball.dir = 360 - ball.dir;
+			}
+			
+			if(tar_top_left_dir < 0 && tar_top_left_dir > -90 && tar_bottom_left_dir > 0 && ball.x > tar[a].x - ball.r){
+				tar[a].live = false;
+				ball.x = tar[a].x - ball.r;
+				ball.dir = 180 - ball.dir;
+			}
+			
+			if(tar_top_right_dir < 0 && tar_bottom_right_dir > 90 && ball.x < tar[a].x + tar[a].w + ball.r){
+				tar[a].live = false;
+				ball.x = tar[a].x + tar[a].w + ball.r;
+				ball.dir = 180 - ball.dir;
+			}
+		}
+	}
+	
+	for(var a = 0; a < num; a++){
+		if(tar[a].live == false){
+			count++;
+		}
+	}
+	
+	if(count == num){
+		ball.vell += 1;
+		base.vell += 1;
+		num += 6;
+		count = 0;
+		sub_x = 0;
+		sub_y = 2;
+		for(var a = 0; a < num; a++){
+			if(sub_x == 6){
+				sub_y += 39;
+				sub_x = 0;
+			}
+			tar[a] = new Object();
+			tar[a].x = 2 + sub_x*83;
+			tar[a].y = sub_y;
+			tar[a].w = 81;
+			tar[a].h = 37;
+			tar[a].live = true;
+			tar[a].color = "green";
+			sub_x++;
+		}
+		ball.y = 37*(num/5) + ball.r + 1;
+		ball.x = base.x + base.width/2;
+		ball.dir = 90;
+	}
+	
+	//BASE movement
+	if(m_r == true && base.x < c1.width - base.width){
+		base.x += base.vell;
+	}
+	
+	if(m_l == true && base.x > 0){
+		base.x -= base.vell;
+	}
+
+},17);
